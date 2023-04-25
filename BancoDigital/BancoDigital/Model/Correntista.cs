6088aa1195eecd086b684a01eed502f7b84e6012
mahteixeira <teixeiramarina13@gmail.com>
2023-04-25 14:31:28 -0300

    Adicionar arquivos de projeto.

---
+++ BancoDigital/BancoDigital/Model/Correntista.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace BancoDigital.Model
+{
+    public class Correntista
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string data_nasc { get; set; }
+        public string cpf { get; set; }
+        public string senha { get; set; }
+    }
+}
