953de657723a87b901b8cb0900e1e240e88a5353
mahteixeira <teixeiramarina13@gmail.com>
2023-09-26 10:48:29 -0300

    area pix pt2

---
+++ BancoDigital/BancoDigital/View/Pags/Pix/Pagar/PagarComChave.xaml.cs
@@ -0,0 +1,23 @@
+﻿using BancoDigital.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BancoDigital.View.Pags.Pix.Pagar
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PagarComChave : ContentPage
+    {
+        public PagarComChave()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            canto.Source = ImageSource.FromResource("BancoDigital.Imagens.canto.png");
+        }
+    }
+}
--- BancoDigital/BancoDigital/View/Pags/Pix/PixMenu.xaml.cs
+++ BancoDigital/BancoDigital/View/Pags/Pix/PixMenu.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,38 @@ namespace BancoDigital.View.Pags.Pix
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
             btn_voltar.Source = ImageSource.FromResource("BancoDigital.Imagens.voltar-Rosa.png");
+            canto2.Source = ImageSource.FromResource("BancoDigital.Imagens.canto2.png");
 
         }
 
         private async void btn_voltar_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new View.Inicio());
+        }
+
+        private async void btn_pagar_qr_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new View.Pags.Pix.LerQRCode());  
+        }
+
+        private async void btn_pagar_chave_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new View.Pags.Pix.Pagar.PagarComChave());
+        }
+
+        private void btn_pagar_copiaecola_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void btn_receberr_qr_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new View.Pags.Pix.gerarQrCode());
+        }
+
+        private void btn_ver_chaves_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
